93b892bc4febdbdb80b077b5e0080dedc4909a46
Ali Saleem Hasan <ali1saleem1hasan@gmail.com>
2021-06-14 13:31:49 +0300

    add movie information when click

--- src/Row.js
+++ src/Row.js
@@ -1,45 +1,55 @@
 import React, { useState, useEffect } from "react";
 import "./Row.css";
 import axios from "./axios";
-// import YouTube from "react-yotube";
+import movieTrailer from 'movie-trailer' 
+import YouTube from "react-youtube"
 function Row({ big, title, categoruUrl }) {
   const [movies, setMovies] = useState([]);
+  const [movie,setMovie]=useState();
   const url = "https://image.tmdb.org/t/p/w500/";
 
-  // const [trailerUrl, setTrailerUrl] = useState("");
-  // const playTrailer = (movie) => {
-  //   if (trailerUrl) {
-  //     setTrailerUrl("");
-  //   } else {
-  //     movieTrailer(movie?.name || "")
-  //       .then((url) => {
-  //         const urlParam = new URLSearchParams(new URL(url).search).get("v");
-  //         setTrailerUrl(urlParam);
-  //       })
-  //       .catch((error) => alert(error.message));
-  //   }
-  // };
-  // const opt = {
-  //   width: "300",
-  //   height: "150",
-  //   playerVars: {
-  //     autoplay: 1,
-  //   },
-  // };
+
+  const [trailerUrl, setTrailerUrl] = useState("");
+
   useEffect(() => {
     async function fetchdata() {
       const request = await axios.get(categoruUrl);
-
       setMovies(request.data.results);
     }
     fetchdata();
   }, [categoruUrl]);
+  const playTrailer = async(movie) => {
+  console.log(movie)
+    setMovie(movie);
+     await movieTrailer(movie.original_title)
+        .then((url) => {
+      console.log(url)
+
+          const urlParam = new URLSearchParams(new URL(url).search).get("v");
+          setTrailerUrl(urlParam);
+          console.log(urlParam)
+        })
+        .catch((error) => console.log(error.message));
+  };
+  const opt = {
+    width: "300",
+    height: "150",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const _onReady = (e) => {
+    e.target.pauseVideo();
+  };
+  
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__info">
         {movies.map((movie) => (
           <img
+            onClick={() => playTrailer(movie)}
             className={`row__img ${big && "row__big"}`}
             key={movie.id}
             src={
@@ -53,7 +63,28 @@ function Row({ big, title, categoruUrl }) {
           />
         ))}
       </div>
-      {/* <YouTube vedioId={trailerUrl} opts={opt} /> */}
+      {
+        trailerUrl && <div className="movie__info">
+       <div className="info__left">
+          <div className="info">
+            <h3>Title</h3>
+          <p>{movie.title}</p>
+            
+          </div>
+          <div className="info">
+            <h3>Rating</h3>
+            <p>{movie.vote_average}</p>
+
+          </div>
+          <div className="info">
+            <h3>Overview</h3>
+            <p>{movie.overview}</p>
+          </div>
+       </div>
+           <YouTube vedioId={trailerUrl} opts={opt}  onReady={_onReady} />
+        </div>
+      }
+     
     </div>
   );
 }
